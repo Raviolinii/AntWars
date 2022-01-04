9825fb92f1753210f11b6a9fbad3c41c124ed2d1
Krzysztof Garlacz <54943478+Raviolinii@users.noreply.github.com>
2022-01-04 19:26:04 +0100

    Feromon vanishing

--- AntWars/Assets/Scripts/Feromon.cs
+++ AntWars/Assets/Scripts/Feromon.cs
@@ -6,12 +6,16 @@ public class Feromon : MonoBehaviour
 {
     private int amount;
     private float vanishingTime;
-    private int decreseAmount;
+    private int vanishingAmount;
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {        
+        SetFeromonAmount(5);
+        SetVanishingTime(1.5f);
+        SetVanishingAmount(1);
+
+        BeginFeromonVanishing();
     }
 
     // Update is called once per frame
@@ -25,13 +29,23 @@ public class Feromon : MonoBehaviour
     public void AddFeromon(int value) => amount += value;
     public void DecreaseFeromonAmount(int value)
     {
-        if (amount >= value)
+        if (amount > value)
             amount -= value;
         else
-            amount = 0;
+            amount = 0;        
     }
     public void RemoveFeromon() => amount = 0;
+    public float GetVanishingTime() => vanishingTime;
+    public void SetVanishingTime(float value) => vanishingTime = value;
+
+    public int GetVanishingAmount() => vanishingAmount;
+    public void SetVanishingAmount(int value) => vanishingAmount = value;
+
 
     private void BeginFeromonVanishing() => InvokeRepeating("FeromonVanishing", vanishingTime, vanishingTime);
-    private void FeromonVanishing() => DecreaseFeromonAmount(decreseAmount);
+    private void FeromonVanishing()
+    {
+        if (amount > 0)
+            DecreaseFeromonAmount(vanishingAmount);
+    }
 }
